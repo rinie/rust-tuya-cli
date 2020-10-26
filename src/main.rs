37ebfdf1f406f3992ac50d1ae91deebc07faa09e
Emil Sodergren <EmilSodergren@users.noreply.github.com>
2020-10-26 20:20:20 +0100

    Add possibility to set value to socket

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use anyhow::Error;
 use clap::{App, Arg};
+use rust_tuyapi::{payload, tuyadevice::TuyaDevice, TuyaType};
+use std::net::SocketAddr;
 
 type Result<T> = std::result::Result<T, Error>;
 fn main() -> Result<()> {
@@ -14,8 +16,7 @@ fn main() -> Result<()> {
                     Arg::new("key")
                         .about("Tuya key")
                         .long("key")
-                        .takes_value(true)
-                        .required(true),
+                        .takes_value(true),
                 )
                 .arg(Arg::new("id").about("Tuya id").long("id").takes_value(true))
                 .arg(
@@ -24,17 +25,42 @@ fn main() -> Result<()> {
                         .long("ip")
                         .takes_value(true)
                         .required(true),
+                )
+                .arg(
+                    Arg::new("version")
+                        .about("Tuya protocol version [3.1 or 3.3]")
+                        .short('v')
+                        .long("protocol-version")
+                        .takes_value(true)
+                        .default_value("3.3")
+                        .required(false),
+                )
+                .arg(
+                    Arg::new("raw-value")
+                        .about("Tuya protocol version [3.1 or 3.3]")
+                        .long("value")
+                        .takes_value(true)
+                        .required(true),
+                )
+                .arg(
+                    Arg::new("dps")
+                        .about("DPS index to set")
+                        .long("dps")
+                        .takes_value(true)
+                        .default_value("1"),
                 ),
         )
         .get_matches();
 
     if let Some(ref matches) = matches.subcommand_matches("set") {
-        println!(
-            "Value of ip: {}, key: {}",
-            matches.value_of("ip").unwrap(),
-            matches.value_of("key").unwrap()
-        );
+        let version = matches.value_of("version").unwrap();
+        let key = matches.value_of("key");
+        let ip: SocketAddr = matches.value_of("key").unwrap().parse()?;
+        let id = matches.value_of("id").unwrap();
+        let value = matches.value_of("raw-value").unwrap();
+        let td = TuyaDevice::create(version, key, ip)?;
+        let payload = payload(id, TuyaType::Socket, value)?;
+        td.set(&payload, 0)?;
     };
-
     Ok(())
 }
